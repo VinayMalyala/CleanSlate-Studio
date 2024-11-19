92b69679aeef154e651baa53484bce115dd9129a
Vinay Malyala <srivinay1432@gmail.com>
2024-11-19 20:01:54 +0530

    working on backend

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+
+//* App Config
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+//* Initialise Middleware
+app.use(express.json());
+app.use(cors());
+
+
+//* API routes
+app.get('/', (req, res) => res.send("API Working"));
+
+app.listen(PORT, () => {
+    console.log('Server is running on port', PORT);
+});
